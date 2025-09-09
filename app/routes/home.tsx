0ca84f70c92f62634a2acb14247c251035017d15
Justin Hollis <justin.hollis@wilder.org>
2025-09-08 19:55:19 -0500

    Troubleshooting

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,5 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import React from "react";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+const Home = () => <h1>Home Page</h1>;
 
-export default function Home() {
-  return <Welcome />;
-}
+export default Home;
